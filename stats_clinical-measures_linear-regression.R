90d5e8858ca6d9770a8d944bfc0d4810df786669
ImogenL <imogen.e.leaning@gmail.com>
2024-12-04 09:58:56 +0100

    Added interaction between group and dwell time

--- stats_clinical-measures_linear-regression.R
+++ stats_clinical-measures_linear-regression.R
@@ -9,28 +9,16 @@ df_MMSEsubset <- subset(df, !is.na(df$MMSE_TOT))
 df_MMSEsubset <- subset(df_MMSEsubset, !is.na(df_MMSEsubset$LABEL)) 
 df_MMSEsubset$MMSE_TOT <- as.numeric(as.character(df_MMSEsubset$MMSE_TOT))
 df_MMSEsubset$AGE <- as.numeric(as.character(df_MMSEsubset$AGE))
-model <- lm(MMSE_TOT ~ dwell_times_state1 + AGE, data = df_MMSEsubset)
 
-# For the different labels:
+# For the different labels
 df_MMSEsubset$LABEL <- as.character(df_MMSEsubset$LABEL)
 df_MMSEsubset$LABEL[df_MMSEsubset$LABEL == 'Control'] <- 0
 df_MMSEsubset$LABEL[df_MMSEsubset$LABEL == 'Alzheimer\'s disease'] <- 1
 df_MMSEsubset$LABEL <- as.factor(df_MMSEsubset$LABEL)
 
-df_MMSEsubset_HC <- subset(df_MMSEsubset, LABEL==0)
-df_MMSEsubset_AD <- subset(df_MMSEsubset, LABEL==1)
-
-model_HC <- lm(MMSE_TOT ~ dwell_times_state1 + AGE, data = df_MMSEsubset_HC)
-model_AD <- lm(MMSE_TOT ~ dwell_times_state1 + AGE, data = df_MMSEsubset_AD)
-
-model_all_testing <- tidy(model)
-model_HC <- tidy(model_HC)
-model_AD <- tidy(model_AD)
-data_list <- list(model_all_testing, model_HC, model_AD)
-all_mmse_models <- rbindlist(data_list)
-number_of_models <- 3
-all_mmse_models$FDR.corrected.p.value <- as.numeric(as.character(lapply(all_mmse_models$p.value, function(x) p.adjust(x, method = "fdr", n = number_of_models))))
-write.table(all_mmse_models, file = "/directory/file_name.txt", sep = ",", quote = FALSE, row.names = F)
+model <- lm(MMSE_TOT ~ AGE + dwell_times_state2*LABEL, data = df_MMSEsubset)
+model <- tidy(model)
+write.table(model, file = "/directory/file_name.txt", sep = ",", quote = FALSE, row.names = F)
 
 ### Models for PANSS scores
 df$PANSS_PT[df$PANSS_PT == 'empty'] <- NA  # non SZ participants
@@ -44,11 +32,11 @@ df_PANSSsubset$PANSS_GT <- as.numeric(as.character(df_PANSSsubset$PANSS_GT))
 df_PANSSsubset$PANSS_CS <- as.numeric(as.character(df_PANSSsubset$PANSS_CS))
 df_PANSSsubset$PANSS_TOTAL <- df_PANSSsubset$PANSS_PT + df_PANSSsubset$PANSS_NT + df_PANSSsubset$PANSS_GT # Calculate total score
 df_PANSSsubset$AGE <- as.numeric(as.character(df_PANSSsubset$AGE))
-model_PT <- lm(PANSS_PT ~ dwell_times_state1 + AGE, data = df_PANSSsubset)
-model_NT <- lm(PANSS_NT ~ dwell_times_state1 + AGE, data = df_PANSSsubset)
-model_GT <- lm(PANSS_GT ~ dwell_times_state1 + AGE, data = df_PANSSsubset)
-model_CS <- lm(PANSS_CS ~ dwell_times_state1 + AGE, data = df_PANSSsubset)
-model_TOTAL <- lm(PANSS_TOTAL ~ dwell_times_state1 + AGE, data = df_PANSSsubset)
+model_PT <- lm(PANSS_PT ~ dwell_times_state2 + AGE, data = df_PANSSsubset)
+model_NT <- lm(PANSS_NT ~ dwell_times_state2 + AGE, data = df_PANSSsubset)
+model_GT <- lm(PANSS_GT ~ dwell_times_state2 + AGE, data = df_PANSSsubset)
+model_CS <- lm(PANSS_CS ~ dwell_times_state2 + AGE, data = df_PANSSsubset)
+model_TOTAL <- lm(PANSS_TOTAL ~ dwell_times_state2 + AGE, data = df_PANSSsubset)
 
 model_PT <- tidy(model_PT)
 model_NT <- tidy(model_NT)
--- stats_social-measures_linear-regression.R
+++ stats_social-measures_linear-regression.R
@@ -11,10 +11,7 @@ df_SFSsubset$SFS <- as.numeric(as.character(df_SFSsubset$SFS))
 df_SFSsubset$LONELINESS <- as.numeric(as.character(df_SFSsubset$LONELINESS))
 df_SFSsubset$AGE <- as.numeric(as.character(df_SFSsubset$AGE))
 
-# Overall testing
-model_all_testing <- lm(SFS ~ dwell_times_state1 + AGE, data = df_SFSsubset)
-
-# For the different labels:
+# For the different labels
 df_SFSsubset$LABEL <- as.character(df_SFSsubset$LABEL)
 df_SFSsubset$LABEL[df_SFSsubset$LABEL == 'Control'] <- 0
 df_SFSsubset$LABEL[df_SFSsubset$LABEL == 'Schizophrenia'] <- 1
@@ -23,21 +20,25 @@ df_SFSsubset$LABEL[df_SFSsubset$LABEL == 'Alzheimers disease'] <- 2
 df_SFSsubset$LABEL[df_SFSsubset$LABEL == 'Dementia'] <- 2
 df_SFSsubset$LABEL <- as.factor(df_SFSsubset$LABEL)
 
+# Overall testing
+model_all_testing <- lm(SFS ~ AGE + LABEL*dwell_times_state2, data = df_SFSsubset)
+model_all_testing <- tidy(model_all_testing)
+write.table(model_all_testing, file = "/directory/file_name.txt", sep = ",", quote = FALSE, row.names = F)
+
+# For each label
 df_SFSsubset_HC <- subset(df_SFSsubset, LABEL==0)
 df_SFSsubset_SZ <- subset(df_SFSsubset, LABEL==1)
 df_SFSsubset_AD <- subset(df_SFSsubset, LABEL==2)
 
-model_HC <- lm(SFS ~ dwell_times_state1 + AGE, data = df_SFSsubset_HC)
-model_SZ <- lm(SFS ~ dwell_times_state1 + AGE, data = df_SFSsubset_SZ)
-model_AD <- lm(SFS ~ dwell_times_state1 + AGE, data = df_SFSsubset_AD)
-
-model_all_testing <- tidy(model_all_testing)
+model_HC <- lm(SFS ~ dwell_times_state2 + AGE, data = df_SFSsubset_HC)
+model_SZ <- lm(SFS ~ dwell_times_state2 + AGE, data = df_SFSsubset_SZ)
+model_AD <- lm(SFS ~ dwell_times_state2 + AGE, data = df_SFSsubset_AD)
 model_HC <- tidy(model_HC)
 model_SZ <- tidy(model_SZ)
 model_AD <- tidy(model_AD)
-data_list <- list(model_all_testing, model_HC, model_SZ, model_AD)
+data_list <- list(model_HC, model_SZ, model_AD)
 all_SFS_models <- rbindlist(data_list)
-number_of_models <- 4
+number_of_models <- 3
 all_SFS_models$FDR.corrected.p.value <- as.numeric(as.character(lapply(all_SFS_models$p.value, function(x) p.adjust(x, method = "fdr", n = number_of_models))))
 write.table(all_SFS_models, file = "/directory/file_name.txt", sep = ",", quote = FALSE, row.names = F)
 
@@ -47,9 +48,8 @@ df_LONELINESSsubset <- subset(df, !is.na(df$LONELINESS))
 df_LONELINESSsubset <- subset(df_LONELINESSsubset, !is.na(df_LONELINESSsubset$LABEL))
 df_LONELINESSsubset$LONELINESS <- as.numeric(as.character(df_LONELINESSsubset$LONELINESS))
 df_LONELINESSsubset$AGE <- as.numeric(as.character(df_LONELINESSsubset$AGE))
-model_all_testing <- lm(LONELINESS ~ dwell_times_state1 + AGE, data = df_LONELINESSsubset)
 
-# For the different labels:
+# For the different labels
 df_LONELINESSsubset$LABEL <- as.character(df_LONELINESSsubset$LABEL)
 df_LONELINESSsubset$LABEL[df_LONELINESSsubset$LABEL == 'Control'] <- 0
 df_LONELINESSsubset$LABEL[df_LONELINESSsubset$LABEL == 'Schizophrenia'] <- 1
@@ -57,19 +57,7 @@ df_LONELINESSsubset$LABEL[df_LONELINESSsubset$LABEL == 'Alzheimer\'s disease'] <
 df_LONELINESSsubset$LABEL[df_LONELINESSsubset$LABEL == 'Alzheimers disease'] <- 2
 df_LONELINESSsubset$LABEL[df_LONELINESSsubset$LABEL == 'Dementia'] <- 2
 df_LONELINESSsubset$LABEL <- as.factor(df_LONELINESSsubset$LABEL)
-
-df_LONELINESSsubset_HC <- subset(df_LONELINESSsubset, LABEL==0)
-df_LONELINESSsubset_SZ <- subset(df_LONELINESSsubset, LABEL==1)
-df_LONELINESSsubset_AD <- subset(df_LONELINESSsubset, LABEL==2)
-
-model_HC <- lm(LONELINESS ~ dwell_times_state1 + AGE, data = df_LONELINESSsubset_HC)
-model_SZ <- lm(LONELINESS ~ dwell_times_state1 + AGE, data = df_LONELINESSsubset_SZ)
-model_AD <- lm(LONELINESS ~ dwell_times_state1 + AGE, data = df_LONELINESSsubset_AD)
-
+                                                                       
+model_all_testing <- lm(LONELINESS ~ AGE + LABEL*dwell_times_state2, data = df_LONELINESSsubset)
 model_all_testing <- tidy(model_all_testing)
-model_HC <- tidy(model_HC)
-model_SZ <- tidy(model_SZ)
-model_AD <- tidy(model_AD)
-data_list <- list(model_all_testing, model_HC, model_SZ, model_AD)
-all_LONELINESS_models <- rbindlist(data_list)
-write.table(all_LONELINESS_models, file = "/directory/file_name.txt", sep = ",", quote = FALSE, row.names = F)
+write.table(model_all_testing, file = "/directory/file_name.txt", sep = ",", quote = FALSE, row.names = F)
